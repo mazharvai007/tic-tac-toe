d94f8343b18193cf25a855f9fb229848623dcbd6
mazhar <swopnil420@gmail.com>
2024-04-06 00:06:49 +0600

    tic tac toe games basic wireframe has been completed. It time to make the game and set the winner.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import Board from './components/Board';
 
 function App() {
 	return (
 		<div className="App">
-			<h1>Tic Tac Toe</h1>
+			<div className="wrapper">
+				<h1>Tic Tac Toe</h1>
+				<Board />
+			</div>
 		</div>
 	);
 }

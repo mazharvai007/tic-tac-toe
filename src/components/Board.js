e4f7f5481bc7d5b1a015972244446872725827c0
mazhar <swopnil420@gmail.com>
2024-04-06 10:12:40 +0600

    Finally we announced the winner

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,8 +5,13 @@ function Board() {
 	const [xIsNext, setXIsNext] = useState(true);
 	const [squares, setSquares] = useState(Array(9).fill(null));
 
+	/**
+	 * Changed the box values by clicking
+	 * @param {*} i
+	 * @returns
+	 */
 	function handleClick(i) {
-		if (squares[i]) {
+		if (squares[i] || calculateWinner(squares)) {
 			return;
 		}
 
@@ -21,8 +26,49 @@ function Board() {
 		setXIsNext(!xIsNext);
 	}
 
+	/**
+	 * The function will announce who is win
+	 * @param {*} squares
+	 * @returns
+	 */
+
+	function calculateWinner(squares) {
+		const lines = [
+			[0, 1, 2],
+			[3, 4, 5],
+			[6, 7, 8],
+			[0, 3, 6],
+			[1, 4, 7],
+			[2, 5, 8],
+			[0, 4, 8],
+			[2, 4, 6],
+		];
+
+		for (let i = 0; i < lines.length; i++) {
+			const [a, b, c] = lines[i];
+			if (
+				squares[a] &&
+				squares[a] === squares[b] &&
+				squares[a] === squares[c]
+			) {
+				return squares[a];
+			}
+		}
+
+		return null;
+	}
+
+	const winner = calculateWinner(squares);
+	let status;
+	if (winner) {
+		status = `Winner: ${winner}`;
+	} else {
+		status = `Next Player: ${xIsNext ? 'X' : 'O'}`;
+	}
+
 	return (
 		<>
+			<div className="status">{status}</div>
 			<div className="board-row">
 				<Square
 					value={squares[0]}

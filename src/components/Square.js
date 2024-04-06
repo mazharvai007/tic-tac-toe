7582f7d50c875a76ec1e11404cb8f5bdc5771499
mazhar <swopnil420@gmail.com>
2024-04-06 09:26:42 +0600

    State lifting up to maintain the state is changed when click

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,14 +1,8 @@
 import React, { useState } from 'react';
 
-function Square() {
-	const [value, setValue] = useState(null);
-
-	function handleClick() {
-		setValue('X');
-	}
-
+function Square({ value, onSquareClick }) {
 	return (
-		<button onClick={handleClick} className="square">
+		<button onClick={onSquareClick} className="square">
 			{value}
 		</button>
 	);
